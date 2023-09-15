10373142b9674aca360c0543a500b8b4dbc725a3
jacobmorrison <iamjacobmorrison@gmail.com>
2023-09-15 16:36:52 -0400

    reroll dice and remove dice on screen

--- ARDicee/ViewController.swift
+++ ARDicee/ViewController.swift
@@ -10,7 +10,9 @@ import SceneKit
 import ARKit
 
 class ViewController: UIViewController, ARSCNViewDelegate {
-
+    
+    var diceArray = [SCNNode]()
+    
     @IBOutlet var sceneView: ARSCNView!
     
     override func viewDidLoad() {
@@ -20,36 +22,30 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         
         // Set the view's delegate
         sceneView.delegate = self
-                
+        
         // width, height, length are in meters, chamferRadius is how rounded you want the corners
         //let cube = SCNBox(width: 0.1, height: 0.1, length: 0.1, chamferRadius: 0.01)
-//        let sphere = SCNSphere(radius: 0.2)
-//
-//        let material = SCNMaterial()
-//        //diffuse is base material of object
-//        //material.diffuse.contents = UIColor.red
-//        material.diffuse.contents = UIImage(named: "art.scnassets/8k_moon.jpg")
-//        //cube.materials = [material]
-//        sphere.materials = [material]
-//
-//        // create node, point in 3D space
-//        let node = SCNNode()
-//        node.position = SCNVector3(x: 0, y: 0.1, z: -0.5)
-//        //node.geometry = cube
-//        node.geometry = sphere
-//
-//        // put node in scenceView
-//        sceneView.scene.rootNode.addChildNode(node)
+        //        let sphere = SCNSphere(radius: 0.2)
+        //
+        //        let material = SCNMaterial()
+        //        //diffuse is base material of object
+        //        //material.diffuse.contents = UIColor.red
+        //        material.diffuse.contents = UIImage(named: "art.scnassets/8k_moon.jpg")
+        //        //cube.materials = [material]
+        //        sphere.materials = [material]
+        //
+        //        // create node, point in 3D space
+        //        let node = SCNNode()
+        //        node.position = SCNVector3(x: 0, y: 0.1, z: -0.5)
+        //        //node.geometry = cube
+        //        node.geometry = sphere
+        //
+        //        // put node in scenceView
+        //        sceneView.scene.rootNode.addChildNode(node)
         
         sceneView.autoenablesDefaultLighting = true
         
-//        // Create a new scene
-//        let diceScene = SCNScene(named: "art.scnassets/diceCollada.scn")!
-//        // withName is name from the attribute inspector from the 'diceCollada' file in the 'art' folder
-//        if let diceNode = diceScene.rootNode.childNode(withName: "Dice", recursively: true) {
-//            diceNode.position = SCNVector3(x: 0, y: 0, z: -0.1)
-//            sceneView.scene.rootNode.addChildNode(diceNode)
-//        }
+        
     }
     
     override func viewWillAppear(_ animated: Bool) {
@@ -59,7 +55,7 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         let configuration = ARWorldTrackingConfiguration()
         
         configuration.planeDetection = .horizontal
-
+        
         // Run the view's session
         sceneView.session.run(configuration)
     }
@@ -72,37 +68,84 @@ class ViewController: UIViewController, ARSCNViewDelegate {
     }
     
     // This method gets called when a touch is detected in the view or window
-//    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
-//        if let touch = touches.first {
-//            let touchLocation = touch.location(in: sceneView)
-//
-//            // convert 2D touch location to 3D
-//            let results = sceneView.hitTest(touchLocation, types: .existingPlaneUsingExtent)
-//
-//            if !results.isEmpty {
-//                print("touched the plane")
-//            } else {
-//                print("touched somewhere else")
-//            }
-//        }
-//    }
+    //    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+    //        if let touch = touches.first {
+    //            let touchLocation = touch.location(in: sceneView)
+    //
+    //            // convert 2D touch location to 3D
+    //            let results = sceneView.hitTest(touchLocation, types: .existingPlaneUsingExtent)
+    //
+    //            if !results.isEmpty {
+    //                print("touched the plane")
+    //            } else {
+    //                print("touched somewhere else")
+    //            }
+    //        }
+    //    }
     
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
         guard let touch = touches.first else { return }
-
+        
         let touchLocation = touch.location(in: sceneView)
-
+        
         if let query = sceneView.raycastQuery(from: touchLocation, allowing: .estimatedPlane, alignment: .any) {
             let results = sceneView.session.raycast(query)
             
-            if !results.isEmpty {
-                print("touched the plane")
-            } else {
-                print("touched somewhere else")
+            if let hitResult = results.first {
+                // Create a new scene
+                let diceScene = SCNScene(named: "art.scnassets/diceCollada.scn")!
+                // withName is name from the attribute inspector from the 'diceCollada' file in the 'art' folder
+                if let diceNode = diceScene.rootNode.childNode(withName: "Dice", recursively: true) {
+                    diceNode.position = SCNVector3(
+                        x: hitResult.worldTransform.columns.3.x,
+                        y: hitResult.worldTransform.columns.3.y + diceNode.boundingSphere.radius,
+                        z: hitResult.worldTransform.columns.3.z
+                    )
+                    
+                    diceArray.append(diceNode)
+                    sceneView.scene.rootNode.addChildNode(diceNode)
+                    
+                    roll(dice: diceNode)
+                }
             }
         }
     }
-
+    
+    func rollAll() {
+        if !diceArray.isEmpty {
+            for dice in diceArray {
+                roll(dice: dice)
+            }
+        }
+    }
+    
+    func roll (dice: SCNNode) {
+        let randomX = Float(arc4random_uniform(4) + 1) * (Float.pi/2)
+        let randomZ = Float(arc4random_uniform(4) + 1) * (Float.pi/2)
+        dice.runAction(
+            SCNAction.rotateBy(
+                x: CGFloat(randomX * 5),
+                y: 0,
+                z: CGFloat(randomZ * 5),
+                duration: 0.5))
+    }
+    
+    @IBAction func rollAgain(_ sender: UIBarButtonItem) {
+        rollAll()
+    }
+    
+    // method triggered when device is shaken
+    override func motionEnded(_ motion: UIEvent.EventSubtype, with event: UIEvent?) {
+        rollAll()
+    }
+    
+    @IBAction func removeAllDice(_ sender: UIBarButtonItem) {
+        if !diceArray.isEmpty {
+            for dice in diceArray {
+                dice.removeFromParentNode()
+            }
+        }
+    }
     
     // when a horizontal plane is detected this method will be called. anchor parameter will be used to place objects in the real world
     func renderer(_ renderer: SCNSceneRenderer, didAdd node: SCNNode, for anchor: ARAnchor) {
